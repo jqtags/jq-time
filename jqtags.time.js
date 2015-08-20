cdbe7d20eadaf8059a1554b67698487f0bb5bc5b
animesh <animesh@unicommerce.com>
2015-08-20 15:04:03 +0530

    changes in the type = until

--- jqtags.time.js
+++ jqtags.time.js
@@ -17,35 +17,88 @@ _tag_('jqtags.time', function (test) {
         type: "string",
         default: ""
       },
+      live: {
+        type: "string"
+      },
       format: {
         type: "string",
         default: "D/MM/YYYY"
       }
     },
     attachedCallback: function () {
+      var self = this;
       this.setValue();
+      if(this.$.live !== undefined){
+        self.updateTime();
+      }
+    },
+    updateTime : function(){
+      var self= this;
+      self.setValue();
+      switch(self.currentDiff.unit){
+        case "m":
+          self.nextInterval = 30000;
+          break;
+        case "s":
+          self.nextInterval = 1000;
+          break
+        default:
+          self.nextInterval = 2700000; //45 minutes
+          break;
+      }
+      window.clearTimeout(self.timer);
+      self.timer = window.setTimeout(function(){
+        self.updateTime();
+      },self.nextInterval);
+    },
+    detachedCallback: function(){
+      window.clearTimeout(this.timer);
     },
     setValue: function () {    //for now assuming only two cases of time input: ISO and UTC
+      var self = this;
       if(isNaN(this.$.value)){ //case for UTC time format
         this.dateTime = new Date(this.$.value);
       }
       else{                    //case for ISO date format
         this.dateTime = new Date(parseInt(this.$.value));
       }
+
       if(this.$.type !== "") {
         //custom types: they'll override format
         switch (this.$.type) {
+          //chat-timestamp
           case "chat-timestamp":
             if (moment().startOf('day').diff(parseInt(this.$.value), 'hours') < 0) {
               this.$.innerHTML = moment(this.dateTime).format("h:mma");
             }
             else {
-              this.$.innerHTML = moment(this.dateTime).format("D/MM/YYYY, h:mma");
+              this.$.innerHTML = moment(this.dateTime).calendar();
             }
             break;
+          //until
           case "until":
-            var timeUntil = moment(this.dateTime).toNow(true).split(" ");
-            this.$.innerHTML = moment(this.dateTime).toNow(true) === "Invalid date" ? "-" : timeUntil[0] + timeUntil[1].split("")[0];
+            var timeDiff = moment.duration(moment(this.dateTime).diff(moment()));
+            self.timeDiffObj = {
+              y: timeDiff.years(),
+              M: timeDiff.months(),
+              d: timeDiff.days(),
+              h: timeDiff.hours(),
+              m: timeDiff.minutes(),
+              s: timeDiff.seconds()
+            };
+            self.currentDiff = {};
+            for(var unit in self.timeDiffObj){
+              if(self.timeDiffObj[unit] !== 0){
+                this.$.innerHTML = self.timeDiffObj[unit] + unit;
+                self.currentDiff.time = self.timeDiffObj[unit];
+                self.currentDiff.unit = unit;
+                console.log(self.currentDiff);
+                break;
+              }
+              else{
+                this.$.innerHTML = "overdue";
+              }
+            }
             break;
         }
       }
