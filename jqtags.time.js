71fcbbde4bc5d13a42460231e1ea76dd00522929
animesh <animesh@unicommerce.com>
2015-08-20 09:56:56 +0530

    Only accepting UTC and ISO format as of now

--- jqtags.time.js
+++ jqtags.time.js
@@ -15,34 +15,44 @@ _tag_('jqtags.time', function (test) {
       },
       type: {
         type: "string",
-        default: "DD/MM/YYYY"
+        default: ""
+      },
+      format: {
+        type: "string",
+        default: "D/MM/YYYY"
       }
     },
     attachedCallback: function () {
       this.setValue();
     },
-    setValue: function () {
-      switch(this.$.type){
-        case "DD/MM/YYYY":
-          this.$.innerHTML = moment(parseInt(this.$.value)).format("D/MM/YYYY");
-          break;
-        case "MM/DD/YYYY":
-          this.$.innerHTML = moment(parseInt(this.$.value)).format("MM/D/YYYY");
-          break;
-        case "chat-timestamp":
-          if(moment().diff(parseInt(this.$.value), 'days') === 0){
-            this.$.innerHTML = moment(parseInt(this.$.value)).format("h:mma");
-          }
-          else{
-            this.$.innerHTML = moment(parseInt(this.$.value)).format("D/MM/YYYY, h:mma");
-          }
-          break;
-        case "until":
-          var timeUntil = moment(parseInt(this.$.value)).toNow(true).split(" ");
-          this.$.innerHTML = timeUntil[0] + timeUntil[1].split("")[0];
-          break;
+    setValue: function () {    //for now assuming only two cases of time input: ISO and UTC
+      if(isNaN(this.$.value)){ //case for UTC time format
+        this.dateTime = new Date(this.$.value);
       }
-
+      else{                    //case for ISO date format
+        this.dateTime = new Date(parseInt(this.$.value));
+      }
+      if(this.$.type !== "") {
+        //custom types: they'll override format
+        switch (this.$.type) {
+          case "chat-timestamp":
+            if (moment().diff(parseInt(this.$.value), 'days') === 0) {
+              this.$.innerHTML = moment(this.dateTime).format("h:mma");
+            }
+            else {
+              this.$.innerHTML = moment(this.dateTime).format("D/MM/YYYY, h:mma");
+            }
+            break;
+          case "until":
+            var timeUntil = moment(this.dateTime).toNow(true).split(" ");
+            this.$.innerHTML = timeUntil[0] + timeUntil[1].split("")[0];
+            break;
+        }
+      }
+      else{
+        //format
+        this.$.innerHTML = moment(this.dateTime).format(this.$.format);
+      }
     }
   };
 
